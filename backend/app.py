d99232844ca1bf7c787507ce982d3b539ad095e9
Erik Braegelmann <ribagel@amazon.com>
2021-07-18 17:52:09 -0500

    Add caching to the /incidents route

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_caching import Cache
 
 from code_challenge_backend.sr_api_client.client import TrafikApiClient
 
 app = Flask(__name__)
 CORS(app)
+
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
+cache.init_app(app)
 
 @app.route('/healthcheck')
 def healthcheck():
@@ -16,6 +20,7 @@ def hello():
 
 
 @app.route('/incidents')
+@cache.cached(timeout=900)
 def incidents():
     incident_list = TrafikApiClient().list_incidents()
     # can we just return a top-level list? seems nicer
