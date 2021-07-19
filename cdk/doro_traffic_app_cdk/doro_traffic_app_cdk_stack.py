2a59835ee00e29d4d4860ca274391c789fc58a45
Erik Braegelmann <ribagel@amazon.com>
2021-07-19 17:18:31 -0500

    Try forwarding to localhost

--- cdk/doro_traffic_app_cdk/doro_traffic_app_cdk_stack.py
+++ cdk/doro_traffic_app_cdk/doro_traffic_app_cdk_stack.py
@@ -1,5 +1,5 @@
 """AWS CDK module to create ECS infrastructure"""
-from aws_cdk import (core, aws_ecs as ecs, aws_ecr as ecr, aws_ec2 as ec2, aws_iam as iam, aws_ecs_patterns as ecs_patterns)
+from aws_cdk import (core, aws_ecs as ecs, aws_ecr as ecr, aws_ec2 as ec2, aws_iam as iam, aws_ecs_patterns as ecs_patterns, aws_logs as logs)
 
 
 class DoroTrafficAppCdkStack(core.Stack):
@@ -13,10 +13,6 @@ class DoroTrafficAppCdkStack(core.Stack):
         ecr.Repository(self,
                        "doro-traffic-ui",
                        repository_name="doro-traffic-ui")
-        ecr.Repository(self,
-                       "doro-traffic-nginx",
-                       repository_name="doro-traffic-nginx")
-
         ecr.Repository(self,
                        "doro-traffic-nginx",
                        repository_name="doro-traffic-nginx")
@@ -52,18 +48,22 @@ class DoroTrafficAppCdkStack(core.Stack):
                                                     execution_role=execution_role,
                                                     family="doro-traffic-app-task-definition")
 
+        logDetail = logs.LogGroup(self, "ecs", log_group_name="ecs", retention=logs.RetentionDays.SIX_MONTHS, removal_policy=core.RemovalPolicy.DESTROY)
+
         nginx_container = task_definition.add_container(
             "nginx",
 
             image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+            logging=ecs.LogDriver.aws_logs(stream_prefix="nginx", log_group=logDetail)
+
         )
         nginx_container_port_mapping = ecs.PortMapping(container_port=80)
         nginx_container.add_port_mappings(nginx_container_port_mapping)
 
         ui_container = task_definition.add_container(
-
             "ui",
             image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+            logging=ecs.LogDriver.aws_logs(stream_prefix="ui", log_group=logDetail)
         )
         ui_container_port_mapping = ecs.PortMapping(container_port=3000)
         ui_container.add_port_mappings(ui_container_port_mapping)
@@ -71,10 +71,10 @@ class DoroTrafficAppCdkStack(core.Stack):
         backend_container = task_definition.add_container(
             "backend",
             image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+            logging=ecs.LogDriver.aws_logs(stream_prefix="backend", log_group=logDetail)
         )
         backend_container_port_mapping = ecs.PortMapping(container_port=5000)
         backend_container.add_port_mappings(backend_container_port_mapping)
-
 
         service = ecs_patterns.ApplicationLoadBalancedFargateService(self,
                                                            "doro-traffic-app-service",
